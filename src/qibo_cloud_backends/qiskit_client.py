8c648d97b634bdfbebac00525d831a457647a8e9
Andrea Papaluca <andreapapaluca@gmail.com>
2024-01-26 10:43:29 +0400

    fix: small fix to qiskit client

--- src/qibo_cloud_backends/qiskit_client.py
+++ src/qibo_cloud_backends/qiskit_client.py
@@ -2,7 +2,8 @@ import random
 from itertools import repeat
 
 from qibo.backends import NumpyBackend
-from qibo.result import MeasurementOutcomes
+from qibo.config import raise_error
+from qibo.result import MeasurementOutcomes, QuantumState
 from qiskit import QuantumCircuit
 from qiskit_ibm_provider import IBMProvider
 
@@ -16,18 +17,24 @@ class QiskitClientBackend(NumpyBackend):
         platform (str): The IBM platform.
     """
 
-    def __init__(self, token, provider="ibm-q", platform="ibmq_qasm_simulator"):
+    def __init__(self, token, provider=None, platform=None):
         super().__init__()
+        if not provider:
+            provider = "ibm-q"
+        if not platform:
+            platform = "ibmq_qasm_simulator"
         provider = IBMProvider(token)
         self.backend = provider.get_backend(platform)
 
-    def execute_circuit(self, circuit, nshots=1000, **kwargs):
+    def execute_circuit(self, circuit, initial_state=None, nshots=1000, **kwargs):
         measurements = circuit.measurements
+        if not measurements:
+            raise_error(RuntimeError, "No measurement found in the provided circuit.")
         nqubits = circuit.nqubits
         circuit = QuantumCircuit.from_qasm_str(circuit.to_qasm())
-        job = self.backend.run(circuit, shots=nshots, **kwargs)
+        result = self.backend.run(circuit, shots=nshots, **kwargs).result()
         samples = []
-        for state, count in job.result().get_counts().items():
+        for state, count in result.get_counts().items():
             sample = [int(bit) for bit in reversed(state)]
             samples += list(repeat(sample, count))
         random.shuffle(samples)
