476c26d528f1128ad626fb487b9411414a28879d
Andrea Papaluca <andreapapaluca@gmail.com>
2024-02-27 16:17:04 +0400

    feat: implemented MetaBackend

--- src/qibo_cloud_backends/__init__.py
+++ src/qibo_cloud_backends/__init__.py
@@ -4,3 +4,45 @@ from qibo_cloud_backends.qibo_client import QiboClientBackend
 from qibo_cloud_backends.qiskit_client import QiskitClientBackend
 
 __version__ = im.version(__package__)
+
+QibocloudBackend = QiboClientBackend | QiskitClientBackend
+
+WORKERS = ("qibo-client", "qiskit")
+
+
+class MetaBackend:
+    """Meta-backend class which takes care of loading the qibojit backends."""
+
+    @staticmethod
+    def load(
+        worker: str, token: str, provider: str = None, platform: str = None
+    ) -> QibocloudBackend:
+        """Loads the backend.
+
+        Args:
+            platform (str): Name of the backend to load: either `numba`, `cupy` or `cuquantum`.
+        Returns:
+            qibo.backends.abstract.Backend: The loaded backend.
+        """
+
+        if worker == "qibo-client":
+            return QiboClientBackend(token, provider, platform)
+        elif worker == "qiskit":
+            return QiskitClientBackend(token, provider, platform)
+        else:
+            raise_error(
+                ValueError,
+                f"Unsupported worker, please use one among {WORKERS}.",
+            )
+
+    def list_available(self) -> dict:
+        """Lists all the available qibocloud backends."""
+        available_backends = {}
+        for worker in WORKERS:
+            try:
+                MetaBackend.load(worker=worker, token="")
+                available = True
+            except:
+                available = False
+            available_backends[worker] = available
+        return available_backends
