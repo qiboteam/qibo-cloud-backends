a1e418cf26ac5d5d75084ce89e5b57a4863a5460
Matthew <46650770+mho291@users.noreply.github.com>
2024-04-30 16:16:43 +0800

    Update test_aws.py
    Updated the Client name.

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -13,10 +13,10 @@ def test_aws_client_backend():
     circuit_qibo.add(gates.M(0, 2))
 
     # Local simulator test, does not cost money
-    client = aws_client.AWSClientBackend()
+    client = aws_client.BraketClientBackend()
     # AWS device tests, cost money
-    # AWS = aws_client.AWSClientBackend(device = AwsDevice("arn:aws:braket:eu-west-2::device/qpu/oqc/Lucy"))
-    # AWS = aws_client.AWSClientBackend(device = AwsDevice("arn:aws:braket:::device/quantum-simulator/amazon/sv1"))
+    # AWS = aws_client.BraketClientBackend(device = AwsDevice("arn:aws:braket:eu-west-2::device/qpu/oqc/Lucy"))
+    # AWS = aws_client.BraketClientBackend(device = AwsDevice("arn:aws:braket:::device/quantum-simulator/amazon/sv1"))
 
     local_res = NP_BACKEND.execute_circuit(circuit_qibo)
     remote_res = client.execute_circuit(circuit_qibo)
