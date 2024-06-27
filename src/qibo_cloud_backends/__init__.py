3315ff5e51792a711a8e14fc6530c19625955f00
Matthew <46650770+mho291@users.noreply.github.com>
2024-06-27 12:43:41 +0800

    Added import BraketClientBackend in __init__.py

--- src/qibo_cloud_backends/__init__.py
+++ src/qibo_cloud_backends/__init__.py
@@ -2,5 +2,6 @@ import importlib.metadata as im
 
 from qibo_cloud_backends.qibo_client import QiboClientBackend
 from qibo_cloud_backends.qiskit_client import QiskitClientBackend
+from qibo_cloud_backends.aws_client import BraketClientBackend
 
 __version__ = im.version(__package__)
