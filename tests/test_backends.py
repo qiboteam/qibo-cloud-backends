8b9236b6d92208e6011c5aec5e056de2e9fac329
Stefano Carrazza <stefano.carrazza@cern.ch>
2024-06-28 20:45:00 +0200

    inverting tests ordering

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -19,29 +19,6 @@ QISKIT_TK = os.environ.get("IBMQ_TOKEN")
 QIBO_TK = os.environ.get("QIBO_CLIENT_TII_TOKEN")
 
 
-@pytest.mark.skipif(
-    not (sys.platform == "linux" and sys.version_info[:2] == (3, 11)),
-    reason="Multiple parallel jobs on IBMQ are not supported.",
-)
-def test_qiskit_client_backend():
-    # ibm_osaka's native gates are: ECR, I, RZ, SX, X
-    c = Circuit(3)
-    c.add(gates.X(0))
-    # c.add(gates.ECR(0,1)) # ECR not supported by (our) QASM apparently
-    # c.add(gates.ECR(1,2))
-    c.add(gates.SX(1))
-    c.add(gates.RZ(2, theta=np.pi / 2))
-    c.add(gates.M(0, 2))
-    client = QiskitClientBackend(
-        token=QISKIT_TK, provider="ibm-q", platform="ibm_osaka"
-    )
-    local_res = NP_BACKEND.execute_circuit(c)
-    remote_res = client.execute_circuit(c)
-    NP_BACKEND.assert_allclose(
-        local_res.probabilities(qubits=[0, 2]), remote_res.probabilities(), atol=1e-1
-    )
-
-
 def test_qibo_client_backend():
     c = random_clifford(3, backend=NP_BACKEND)
     c.add(gates.M(0, 2))
@@ -75,3 +52,26 @@ def test_list_available_backends():
         "qibotn": False,
     }
     assert list_available_backends() == available_backends
+
+
+@pytest.mark.skipif(
+    not (sys.platform == "linux" and sys.version_info[:2] == (3, 11)),
+    reason="Multiple parallel jobs on IBMQ are not supported.",
+)
+def test_qiskit_client_backend():
+    # ibm_osaka's native gates are: ECR, I, RZ, SX, X
+    c = Circuit(3)
+    c.add(gates.X(0))
+    # c.add(gates.ECR(0,1)) # ECR not supported by (our) QASM apparently
+    # c.add(gates.ECR(1,2))
+    c.add(gates.SX(1))
+    c.add(gates.RZ(2, theta=np.pi / 2))
+    c.add(gates.M(0, 2))
+    client = QiskitClientBackend(
+        token=QISKIT_TK, provider="ibm-q", platform="ibm_osaka"
+    )
+    local_res = NP_BACKEND.execute_circuit(c)
+    remote_res = client.execute_circuit(c)
+    NP_BACKEND.assert_allclose(
+        local_res.probabilities(qubits=[0, 2]), remote_res.probabilities(), atol=1e-1
+    )
