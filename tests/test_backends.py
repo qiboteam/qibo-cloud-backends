3b58e2f6119f4a6e151e5a80fddd80eedd752027
Matthew <matthew.ho.291@gmail.com>
2024-07-11 11:15:33 +0800

    Commenting out Qiskit and Qibo. Added AWS Backend test.

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -4,30 +4,43 @@ from qibo import gates
 from qibo.backends import NumpyBackend
 from qibo.quantum_info import random_clifford
 
-from qibo_cloud_backends import QiboClientBackend, QiskitClientBackend
+from qibo_cloud_backends import QiboClientBackend, QiskitClientBackend, BraketClientBackend
 
 NP_BACKEND = NumpyBackend()
-QISKIT_TK = os.environ.get("IBMQ_TOKEN")
-QIBO_TK = os.environ.get("QIBO_CLIENT_TII_TOKEN")
+# QISKIT_TK = os.environ.get("IBMQ_TOKEN")
+# QIBO_TK = os.environ.get("QIBO_CLIENT_TII_TOKEN")
 
 
-def test_qiskit_client_backend():
+# def test_qiskit_client_backend():
+#     c = random_clifford(3, backend=NP_BACKEND)
+#     c.add(gates.M(0, 2))
+#     client = QiskitClientBackend(
+#         token=QISKIT_TK, provider="ibm-q", platform="ibmq_qasm_simulator"
+#     )
+#     local_res = NP_BACKEND.execute_circuit(c)
+#     remote_res = client.execute_circuit(c)
+#     NP_BACKEND.assert_allclose(
+#         local_res.probabilities(qubits=[0, 2]), remote_res.probabilities(), atol=1e-1
+#     )
+
+
+# def test_qibo_client_backend():
+#     c = random_clifford(3, backend=NP_BACKEND)
+#     c.add(gates.M(0, 2))
+#     client = QiboClientBackend(token=QIBO_TK, provider="TII")
+#     local_res = NP_BACKEND.execute_circuit(c)
+#     remote_res = client.execute_circuit(c)
+#     NP_BACKEND.assert_allclose(
+#         local_res.probabilities(qubits=[0, 2]),
+#         remote_res.probabilities(qubits=[0, 2]),
+#         atol=1e-1,
+#     )
+
+
+def test_aws_client_backend():
     c = random_clifford(3, backend=NP_BACKEND)
     c.add(gates.M(0, 2))
-    client = QiskitClientBackend(
-        token=QISKIT_TK, provider="ibm-q", platform="ibmq_qasm_simulator"
-    )
-    local_res = NP_BACKEND.execute_circuit(c)
-    remote_res = client.execute_circuit(c)
-    NP_BACKEND.assert_allclose(
-        local_res.probabilities(qubits=[0, 2]), remote_res.probabilities(), atol=1e-1
-    )
-
-
-def test_qibo_client_backend():
-    c = random_clifford(3, backend=NP_BACKEND)
-    c.add(gates.M(0, 2))
-    client = QiboClientBackend(token=QIBO_TK, provider="TII")
+    client = BraketClientBackend(verbatim_circuit=False)
     local_res = NP_BACKEND.execute_circuit(c)
     remote_res = client.execute_circuit(c)
     NP_BACKEND.assert_allclose(
