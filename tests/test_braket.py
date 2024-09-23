4bcadb95d2fac14d02ee5c6c22644e9232e752e5
Matthew <matthew.ho.291@gmail.com>
2024-09-23 21:28:56 +0800

    Removed extra pytest

--- tests/test_braket.py
+++ tests/test_braket.py
@@ -11,28 +11,6 @@ from qibo_cloud_backends.braket_translation import to_braket
 NP_BACKEND = NumpyBackend()
 
 
-def test_aws_client_backend():
-    circuit_qibo = random_clifford(3, backend=NP_BACKEND)
-    circuit_qibo.add(gates.M(0))
-    circuit_qibo.add(gates.M(1))
-    circuit_qibo.add(gates.M(2))
-
-    # Local simulator test, does not cost money
-    client = BraketClientBackend()
-    # AWS device tests, cost money
-    # AWS = aws_client.AWSClientBackend(device = AwsDevice("arn:aws:braket:eu-west-2::device/qpu/oqc/Lucy"))
-    # AWS = aws_client.AWSClientBackend(device = AwsDevice("arn:aws:braket:::device/quantum-simulator/amazon/sv1"))
-
-    local_res = NP_BACKEND.execute_circuit(circuit_qibo)
-    remote_res = client.execute_circuit(circuit_qibo)
-
-    NP_BACKEND.assert_allclose(
-        local_res.probabilities(qubits=[0, 2]),
-        remote_res.probabilities(qubits=[0, 2]),
-        atol=1e-1,
-    )
-
-
 @pytest.mark.parametrize(
     "gate, expected",
     [
