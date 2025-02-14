c0760ebd39782cb9f1c271752587c879a719f1ff
Renato Mello <renato.msf@gmail.com>
2025-02-14 10:26:11 +0400

    functioning version

--- src/qibo_cloud_backends/ionq_client.py
+++ src/qibo_cloud_backends/ionq_client.py
@@ -5,43 +5,47 @@ from qibo.backends import NumpyBackend
 from qibo.config import raise_error
 from qibo.result import MeasurementOutcomes
 from qiskit import QuantumCircuit
-from qiskit_ibm_provider import IBMProvider  # type: ignore
+from qiskit_ionq import IonQProvider  # type: ignore
 
 
-class QiskitClientBackend(NumpyBackend):
-    """Backend for the remote execution of Qiskit circuits on the IBM servers.
+class IonQClientBackend(NumpyBackend):
+    """Backend for the remote execution of Qibo circuits on the IonQ Cloud servers.
 
     Args:
-        token (str): User authentication token. By default this is read from the 'IBMQ_TOKEN' environment variable.
-        platform (str): The IBM platform. Defaults to `"ibm_kyiv"`.
+        token (str, optional): User authentication token. By default this is read from the 'IONQ_TOKEN' environment variable.
+        platform (str, optional ): The IBM platform. Defaults to `"ionq_simulator"`.
     """
 
-    def __init__(self, token=None, platform=None):
+    def __init__(self, token=None, platform=None, **kwargs):
         super().__init__()
         if token is None:
             try:
-                token = os.environ["IBMQ_TOKEN"]
+                token = os.environ["IONQ_TOKEN"]
             except KeyError:  # pragma: no cover
                 raise_error(
                     RuntimeError,
-                    "No token provided. Please explicitely pass the token `token='your_token'` or set the environment variable `IBMQ_TOKEN='your_token'`.",
+                    "No token provided. Please explicitely pass the token `token='your_token'` or set the environment variable `IONQ_TOKEN='your_token'`.",
                 )
         if platform is None:
-            platform = "ibm_kyiv"
-        self.name = "qiskit-client"
-        provider = IBMProvider(token)
+            platform = "ionq_simulator"
+        self.name = "ionq-client"
+        provider = IonQProvider(token)
         self.backend = provider.get_backend(platform)
+
+        # For the classical simulator, options like noise model can be set
+        self.backend.set_options(**kwargs)
 
     def execute_circuit(self, circuit, initial_state=None, nshots=1000, **kwargs):
         """Executes the passed circuit.
 
         Args:
-            circuit (qibo.models.Circuit): The circuit to execute.
-            initial_state (ndarray): The initial state of the circuit. Defaults to `|00...0>`.
-            nshots (int): Total number of shots.
-            kwargs (dict): Additional keyword arguments passed to the qiskit backends' `run()` method.
+            circuit (:class:`qibo.models.Circuit`): Circuit to be executed.
+            initial_state (ndarray, optional): Initial state of the circuit. Defaults to `|00...0>`.
+            nshots (int, optional): Total number of shots. Defaults to :math:`1000`.
+            kwargs (dict, optional): Additional keyword arguments passed to the IonQ backends' `run()` method.
+
         Returns:
-            (qibo.result.MeasurementOutcomes) The outcome of the circuit execution.
+            :class:`qibo.result.MeasurementOutcomes`: Outcome of the circuit execution.
         """
         if initial_state is not None:
             raise_error(
@@ -55,7 +59,7 @@ class QiskitClientBackend(NumpyBackend):
         result = self.backend.run(circuit, shots=nshots, **kwargs).result()
         samples = []
         for state, count in result.get_counts().items():
-            sample = [int(bit) for bit in reversed(state)]
+            sample = [int(bit) for bit in state[::-1].split()]
             samples += list(repeat(sample, count))
         return MeasurementOutcomes(
             measurements, backend=self, samples=self.np.asarray(samples), nshots=nshots
